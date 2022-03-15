3c05e1ffe23c8f909e74234796c5a58fac836e6a
Frashier <75584735+Frashier@users.noreply.github.com>
2022-03-15 17:57:14 +0100

    working login and registering

--- server/models.js
+++ server/models.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    index: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    sessionID: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  sessionID: {
-    type: String,
-  },
-});
-const userModel = mongoose.model("User", userSchema);
+  { autoCreate: true }
+);
+const User = mongoose.model("User", userSchema);
 
-module.exports = { userModel };
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -11,35 +11,64 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/login", (req, res) => {
-  console.log("POST");
-  res.send(crypto.randomUUID());
-});
-
-app.put("/register", (req, res) => {
-  models.userModel.exists({ username: req.body.username }).then((result) => {
+  // Try to login
+  models.User.exists({
+    username: req.body.username,
+    password: req.body.password,
+  }).then((result) => {
+    // If input is valid
     if (result) {
-      res.send("User with this username exists");
+      const sessionID = crypto.randomUUID();
+      models.User.updateOne(
+        { username: req.body.username },
+        { sessionID: sessionID }
+      )
+        .then(() => {
+          res.send(sessionID);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send();
+        });
     } else {
-      const newUser = new models.userModel({
-        username: req.body.username,
-        password: req.body.password,
-      });
-      newUser.save();
-      res.send("User registered");
+      res.status(401).send();
     }
   });
 });
 
+app.put("/register", (req, res) => {
+  // Check if this username is used, register if not
+  models.User.exists({ username: req.body.username })
+    .then((result) => {
+      if (result) {
+        res.send("User with this username exists");
+      } else {
+        const newUser = new models.User({
+          username: req.body.username,
+          password: req.body.password,
+        });
+        newUser.save();
+        res.send("User registered");
+      }
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
+
 async function run() {
+  // Connect to database
   try {
-    mongoose.connect("mongodb://localhost:27017/test", {
+    await mongoose.connect("mongodb://localhost:27017/test", {
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
     });
   } catch (err) {
-    console.log("Database connection error");
+    console.log(err);
     process.exit();
   }
+
+  // Listen on port
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
